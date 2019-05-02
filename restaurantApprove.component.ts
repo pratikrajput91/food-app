9bca3a88c24aa2859e2ce7f6ec188c6a5cd43110
Rajput <pratik-gajendrasing.rajput@capgemini.com>
2019-05-02 15:04:13 +0530

    restro added

---
+++ restaurantApprove.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+
+  orders=[{ 
+    "contact"  :  8275944888, 
+    "Address"   :  "sector 20 Airoli", 
+    "info"      :  "abc",
+    
+}
+  ]
+}
+  
+    
---
+++ restaurantRegistration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl:"./restaurantRegistration.component.html",
+    // selector:"customer-login"
+
+})
+export class RestaurantRegistrationComponent {
+    
+}
